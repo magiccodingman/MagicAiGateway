b20df8d4db73b21c5a08cceaf17e65e3a0eef952
Magic <131926685+magiccodingman@users.noreply.github.com>
2026-07-14 16:33:03 -0400

    Fix pairing controller security namespace

--- MagicAiApi/Controllers/GatewayPairingController.cs
+++ MagicAiApi/Controllers/GatewayPairingController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using SharedMagic.Configuration;
 using SharedMagic.Contracts;
+using SharedMagic.Security;
 
 namespace MagicAiApi.Controllers;
 
